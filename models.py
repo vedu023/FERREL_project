17a8af37a87f8cb5d002692827da96a0c277f205
Analytics-MVIS <analyticsmvis@gmail.com>
2024-01-19 17:49:51 +0530

    in progress..

--- models.py
+++ models.py
@@ -4,19 +4,54 @@ import torchvision.models as m
 
 
 class conv_block(nn.Module):
-    pass  
+    
+    def __init__(self, in_ch, out_ch):
+        super(self, conv_block).__init__()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch), 
+            nn.ReLU6(inplace=True),
+            nn.BatchNorm2d(out_ch),
+            nn.Conv2d(out_ch, out_ch),
+            nn.ReLU6(inplace=True)
+            )
+        
+    def forward(self, x):
+        return self.conv(x)
+    
+    
 
 class Att_block():
-    pass 
+    def __init__(self, Q, V, K):
+        super(Att_block, self).__init__()
+        
+        pass
+    
+    
+
 
 class head():
-    pass 
+    
+    def __init__(self, no_class):
+        super(self, head).__init__()
+        
+        self.head1 = MLP_block()
+        self.head2 = MLP_block()
+        
+        
+    def forward(self, x):
+        h1 = self.head1(x)
+        h2 = self.head2(x)
+        
+        return h1, h2
+        
 
 class neck():
     pass
     
 class MLP_block(nn.Module):
     pass
+    
     
 class model_1st_pass(nn.Module):
     
